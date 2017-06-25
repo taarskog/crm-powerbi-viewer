f6fe8d9073dc6f2ee54f30444a40cdc23f536afb
Trond Aarskog <trond@heiigjen.com>
2017-06-25 21:25:59 +0200

    Update script

--- pbia.js
+++ pbia.js
@@ -5,6 +5,7 @@ let pbiaWorker = function () {
     let item = null;
     while (posts.length > 0 && (item = posts.shift()) != null) {
         if (logToConsole) {
+            this.post(item);
             console.log(`[PBIA*] '${item.category}' '${item.action}' '${item.value == null ? "" : item.value}'`);
         }
     }
@@ -17,6 +18,7 @@ let pbiaWorker = function () {
         for (let i = 0, l = arguments.length; i < l; i++) {
             let item = arguments[i];
             if (logToConsole) {
+                this.post(item);
                 console.log(`[PBIA] '${item.category}' '${item.action}' '${item.value == null ? "" : item.value}'`);
             }
         }
@@ -24,4 +26,15 @@ let pbiaWorker = function () {
     }
     });
 
+    function post(item) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:55000/");
+        xhr.onerror(function(ev) { console.error(ev); });
+        xhr.send({
+            _: "crm-powerbi-viewer",
+            C: item.category,
+            A: item.action,
+            V: item.value
+        });
+    }
 }();
