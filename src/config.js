48f4d6a62b63b9b600b18d7da98628911b756ade
Trond Aarskog <trond@heiigjen.com>
2018-07-17 09:03:50 +0200

    Updated comments and added samples

--- src/config.js
+++ src/config.js
@@ -1,26 +1,23 @@
 var customConfig = {
 	///////////////////////////////
-	//
 	// ITEMS THAT MUST BE CHANGED
-	//
 	///////////////////////////////
 
-	// See documentation on how to register the application in Azure AD and get a client id.
+	// See documentation on how to register the application in Azure AD and get a client id (remember to enclose with quotes - see sample).
+	// Sample: auth_client_id: "12345678-1234-abcd-1234-11aabb22cc",
 	auth_client_id: null,
 
 
 	///////////////////////////////////////////
-	//
 	// CAN USUALLY BE LEFT UNCHANGED 
 	// (unless specified in the documentation)
-	//
 	///////////////////////////////////////////
 
-    // How to perform authentication - valid values are "inline" or "popup". Default is "inline".
+    // How to perform authentication - valid values are "inline" or "popup". Default is "inline" (rember to enclose with quotes).
     // You should typically use "inline" on Dynamics 365 Online with users located in your Azure AD and "popup" when on-premise (might also be required if you have invited external parties to your Azure AD).
 	auth_mode: null,
 
-    // Auto-refresh access - may cause page reload if token cannot be updated silently. Default is true.
+    // Auto-refresh access - may cause page reload if token cannot be updated silently [valid values are true or false]. Default is true.
 	auto_refresh_token: null,
 
 	// Where to cache tokens. Valid values are 'sessionStorage' and 'localStorage'. Default is 'sessionStorage'.
@@ -34,5 +31,6 @@ var customConfig = {
 	log_level: null,
 
 	// Array of custom scripts to load. Functions in these scripts can be referenced for report filtering etc.
+	// Sample: custom_scripts: ["../../new_myfilter?ver=1", "../../demo_/powerbi/filters/account.js?ver=1"]
     custom_scripts: null
 };
